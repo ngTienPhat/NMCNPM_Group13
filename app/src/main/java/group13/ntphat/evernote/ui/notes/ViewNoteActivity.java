150b43d305f129e0180c0035ec6b0fc1dd469f9e
ngTienPhat <ng.tienphat1712122@gmail.com>
2020-01-02 10:54:57 +0700

    add edit note layout

--- app/src/main/java/group13/ntphat/evernote/ui/notes/ViewNoteActivity.java
+++ app/src/main/java/group13/ntphat/evernote/ui/notes/ViewNoteActivity.java
@@ -28,7 +28,7 @@ public class ViewNoteActivity extends AppCompatActivity implements EditorControl
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.note_editor);
+        setContentView(R.layout.note_edit);
     }
 
     @Override
