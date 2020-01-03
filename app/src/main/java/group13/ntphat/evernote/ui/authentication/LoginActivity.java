d19d96ce8d6c8fa0064d6f946213f08c1e1bc2e4
lhvubtqn <lhvubtqn@gmail.com>
2020-01-03 14:36:16 +0700

    add login activity

--- app/src/main/java/group13/ntphat/evernote/ui/authentication/LoginActivity.java
+++ app/src/main/java/group13/ntphat/evernote/ui/authentication/LoginActivity.java
@@ -1,4 +1,32 @@
 package group13.ntphat.evernote.ui.authentication;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import group13.ntphat.evernote.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setTitle("Evernote");
+
+        final EditText editTxtUsername = findViewById(R.id.editTxt_username);
+        final EditText editTxtPassword = findViewById(R.id.editTxt_password);
+        View btn = findViewById(R.id.btn_login);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTxtUsername.getText().toString();
+                String password = editTxtPassword.getText().toString();
+                // Do login
+            }
+        });
+    }
 }
