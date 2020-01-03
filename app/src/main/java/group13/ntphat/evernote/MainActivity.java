74032a27e63b4e78c673d33c9ff7594363d1af1c
ngTienPhat <ng.tienphat1712122@gmail.com>
2020-01-03 15:04:06 +0700

    change nothing

--- app/src/main/java/group13/ntphat/evernote/MainActivity.java
+++ app/src/main/java/group13/ntphat/evernote/MainActivity.java
@@ -89,6 +89,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -184,6 +185,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public void applyTexts(String notebookName) throws JSONException {
         USER.getInstance().addNoteBook(this.getBaseContext(), notebookName);
+
         NotebookFragment.listNotebookAdapter.notifyDataSetChanged();
     }
 }
