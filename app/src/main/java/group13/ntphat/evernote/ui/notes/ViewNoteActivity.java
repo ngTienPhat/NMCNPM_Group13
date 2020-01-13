b1bc190d18d55af6637b092e401cdb85275d5f3a
lamducanhndgv <lamducanhndgv@gmail.com>
2020-01-13 21:01:08 +0700

    Add Speech to Text feature

--- app/src/main/java/group13/ntphat/evernote/ui/notes/ViewNoteActivity.java
+++ app/src/main/java/group13/ntphat/evernote/ui/notes/ViewNoteActivity.java
@@ -3,10 +3,12 @@ package group13.ntphat.evernote.ui.notes;
 import android.Manifest;
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,6 +33,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import group13.ntphat.evernote.Model.DATA;
 import group13.ntphat.evernote.Model.NOTE;
@@ -227,6 +230,11 @@ public class ViewNoteActivity extends AppCompatActivity implements EditorControl
                 addImage(filePath);
             }
         }
+        if (requestCode == REQ_CODE && resultCode == RESULT_OK && data != null)
+        {
+            ArrayList result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            markDEditor.addTextComponent(markDEditor.getNextIndex(), (String) result.get(0));
+        }
     }
     public void addImage(String filePath) {
         markDEditor.insertImage(filePath);
@@ -315,5 +323,21 @@ public class ViewNoteActivity extends AppCompatActivity implements EditorControl
     public void finish() {
         setResult(RESULT_OK);
         super.finish();
+    }
+
+    private final int REQ_CODE = 2101;
+    public void speechtotext(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Need to speek");
+        try {
+            startActivityForResult(intent, REQ_CODE);
+        }
+        catch(ActivityNotFoundException a){
+            Toast.makeText(getApplicationContext(), "Sorry your device not supported", Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
